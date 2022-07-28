95c6c6bc4e61f12ac93175a5e016ceb905f5fd8d
nitvishu <110082637+nitvishu@users.noreply.github.com>
2022-07-28 11:15:32 +0530

    HomePage
    I have completed my home page task plzz review it.....

---
+++ AutoProject/src/main/java/GitHubDemo/AutoProject/App.java
@@ -0,0 +1,13 @@
+package GitHubDemo.AutoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
